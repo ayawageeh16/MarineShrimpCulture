9be2b265612f3cd3f333adc7e156155d047b68d4
ayawageeh16 <ayawageeh16@gmail.com>
2018-06-02 06:04:01 +0200

    Settings is added

---
+++ app/src/main/java/com/marine/shrimp/culture/marineshrimpculture/view/SettingsActivity.java
@@ -0,0 +1,64 @@
+package com.marine.shrimp.culture.marineshrimpculture.view;
+
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
+
+import com.marine.shrimp.culture.marineshrimpculture.R;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment{
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+
+            bindSummaryValue(findPreference("language"));
+        }
+    }
+
+    public static void bindSummaryValue (Preference preference){
+        preference.setOnPreferenceChangeListener(listener);
+        listener.onPreferenceChange(preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext())
+                .getString(preference.getKey(),""));
+    }
+
+    private static Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+          if ( preference instanceof ListPreference){
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+                preference.setSummary(index>0 ? listPreference.getEntries()[index]
+                                      : null);
+
+            }
+            return true;
+        }
+    };
+
+    @Override
+    public void onBackPressed() {
+        NavUtils.navigateUpFromSameTask(this);
+        super.onBackPressed();
+    }
+}
